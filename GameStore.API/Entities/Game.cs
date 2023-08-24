ca5c36d05697fd6f4f92a62d3bef916799fc8c08
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-25 00:38:57 +0700

    Adding Server Side Validation

--- GameStore.API/Entities/Game.cs
+++ GameStore.API/Entities/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.API.Entities
 {
@@ -10,15 +11,29 @@ namespace GameStore.API.Entities
     {
         [JsonPropertyName("id")]
         public int Id { get; set; }
+
         [JsonPropertyName("name")]
+        [Required]
+        [StringLength(100)]
         public required string Name { get; set; }
+
         [JsonPropertyName("genre")]
+        [Required]
+        [StringLength(20)]
         public required string Genre { get; set; }
+
         [JsonPropertyName("price")]
+        [Required]
+        [Range(1, 1000)]
         public required decimal Price { get; set; }
+
         [JsonPropertyName("release_date")]
+        [Required]
         public required DateTime ReleaseDate { get; set; }
+
         [JsonPropertyName("image_uri")]
+        [Url]
+        [StringLength(250)]
         public required string ImageUri { get; set; }
     }
 }
