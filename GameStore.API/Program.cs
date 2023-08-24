ccd922ead9c745d309ac96a575885850501ca6dc
Azie Melza Pratama <aziemp66@gmail.com>
2023-08-24 23:03:28 +0700

    Put Request

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -41,4 +41,43 @@ app.MapPost("/games", (Game game) =>
 	return Results.CreatedAtRoute(GetGameByIdRouteName, new { id = game.Id }, game);
 });
 
+app.MapPut("/games/{id}", async (ctx) =>
+{
+	ctx.Response.ContentType = "application/json";
+	int id = Convert.ToInt32(ctx.Request.RouteValues["id"]);
+
+	Game game;
+	try
+	{
+		game = games.First(g => g.Id == id);
+	}
+	catch (System.Exception)
+	{
+		ctx.Response.StatusCode = 404;
+		await ctx.Response.WriteAsJsonAsync(new { message = "Game not found." });
+		return;
+	}
+
+	Game? UpdateGame = await ctx.Request.ReadFromJsonAsync<Game>();
+	if (UpdateGame is null)
+	{
+		ctx.Response.StatusCode = 400;
+		await ctx.Response.WriteAsJsonAsync(new { message = "Invalid game data." });
+		return;
+	}
+
+	game = new Game
+	{
+		Id = id,
+		Name = UpdateGame.Name,
+		Genre = UpdateGame.Genre,
+		Price = UpdateGame.Price,
+		ReleaseDate = UpdateGame.ReleaseDate,
+		ImageUri = UpdateGame.ImageUri
+	};
+
+	ctx.Response.Headers.Add("Location", $"{ctx.Request.Scheme}://{ctx.Request.Host}/games/{game.Id}");
+	await JsonSerializer.SerializeAsync(ctx.Response.Body, game);
+});
+
 app.Run();
